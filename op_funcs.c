1691efccb403ecb4835078bf1d365dbd6aec095e
jasonmichaelhancock <jasonmichaelhancock@gmail.com>
2018-04-30 03:12:07 +0000

    correct order of ops

--- op_funcs.c
+++ op_funcs.c
@@ -48,10 +48,163 @@ void op_pop(stack_t **stack, unsigned int linecount)
 
       if (*stack == NULL)
       {
-	    printf("L%u: can't pop an empty stack", linecount);
+	    printf("L%u: can't pop an empty stack\n", linecount);
 	    exit(EXIT_FAILURE);
       }
       temp = *stack;
+      *stack = (*stack)->next;
+      free(temp);
+      return;
+}
+void op_swap(stack_t **stack, unsigned int linecount)
+{
+      stack_t *temp;
+      int tempi;
+
+      if (*stack == NULL)
+      {
+            printf("L%u: can't swap, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp = (*stack)->next;
+      if (temp == NULL)
+      {
+            printf("L%u: can't swap, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      tempi = (*stack)->n;
+      (*stack)->n = temp->n;
+      temp->n = tempi;
+      return;
+}
+void op_add(stack_t **stack, unsigned int linecount)
+{
+      stack_t *temp;
+
+      if (*stack == NULL)
+      {
+            printf("L%u: can't add, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp = (*stack)->next;
+      if (temp == NULL)
+      {
+            printf("L%u: can't add, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp->n = ((*stack)->n + temp->n);
+      temp = (*stack);
+      *stack = (*stack)->next;
+      free(temp);
+      return;
+}
+void op_nop(stack_t **stack, unsigned int linecount)
+{
+      (void)stack;
+      (void)linecount;
+
+      return;
+}
+void op_sub(stack_t **stack, unsigned int linecount)
+{
+      stack_t *temp;
+
+      if (*stack == NULL)
+      {
+            printf("L%u: can't sub, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp = (*stack)->next;
+      if (temp == NULL)
+      {
+            printf("L%u: can't sub, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp->n = (temp->n - (*stack)->n);
+      temp = (*stack);
+      *stack = (*stack)->next;
+      free(temp);
+      return;
+}
+void op_mul(stack_t **stack, unsigned int linecount)
+{
+      stack_t *temp;
+
+      if (*stack == NULL)
+      {
+            printf("L%u: can't mul, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp = (*stack)->next;
+      if (temp == NULL)
+      {
+            printf("L%u: can't mul, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp->n = ((*stack)->n * temp->n);
+      temp = (*stack);
+      *stack = (*stack)->next;
+      free(temp);
+      return;
+}
+void op_div(stack_t **stack, unsigned int linecount)
+{
+      stack_t *temp;
+
+      if (*stack == NULL)
+      {
+            printf("L%u: can't div, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      if ((*stack)->n == 0)
+      {
+            printf("L%u: division by zero\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      if ((*stack)->next == NULL)
+      {
+            printf("L%u: can't div, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      if ((*stack)->n == 0)
+      {
+            printf("L%u: division by zero\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp = (*stack)->next;
+      temp->n = (temp->n / (*stack)->n);
+      temp = (*stack);
+      *stack = (*stack)->next;
+      free(temp);
+      return;
+}
+void op_mod(stack_t **stack, unsigned int linecount)
+{
+      stack_t *temp;
+
+      if (*stack == NULL)
+      {
+            printf("L%u: can't mod, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      if ((*stack)->n == 0)
+      {
+            printf("L%u: division by zero\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      if ((*stack)->next == NULL)
+      {
+            printf("L%u: can't div, stack too short\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      if ((*stack)->n == 0)
+      {
+            printf("L%u: division by zero\n", linecount);
+            exit(EXIT_FAILURE);
+      }
+      temp = (*stack)->next;
+      temp->n = (temp->n % (*stack)->n);
+      temp = (*stack);
       *stack = (*stack)->next;
       free(temp);
       return;
